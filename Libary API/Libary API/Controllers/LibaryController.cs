1f330be4035b81c5ea00b8a1cf6c887b8553c600
Peter Le <Peter Le>
2016-12-08 22:07:13 -0500

    hello commit

---
+++ Libary API/Libary API/Controllers/LibaryController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Net;
+using System.Net.Http;
+using Libary_API.Models;
+
+namespace Libary_API.Controllers
+{
+    public class LibaryController : ApiController
+    {
+    }
+}
---
+++ Libary API/Libary API/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Libary_API.Models
+{
+    public class Book
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int YearPublish { get; set; }
+        public bool IsCheckedOut { get; set; }
+        public DateTime LastCheckOutDate { get; set; }
+        public DateTime DueBackDate { get; set; }
+        
+    }
+}
